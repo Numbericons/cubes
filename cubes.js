605a76458fc8af58edf2820e0d1c5cd152784845
Numbericons <zachjoliver@gmail.com>
2019-12-10 12:41:50 -0800

    Setup file and test cases

---
+++ cubes.js
@@ -0,0 +1,9 @@
+function isSumOfCubes(s) {
+
+}
+
+let x = isSumOfCubes("0 9026315 -827&()"); // "0 0 Lucky";
+let y = isSumOfCubes("9026315 -827&()") // "Unlucky"
+
+console.log(`First result should be '0 0 Lucky' and is ${x}`);
+console.log(`Second result should be 'Unlucky' and is ${y}`);
